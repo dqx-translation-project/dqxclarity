e31ff7abcb89b875459e3d280731a7cba2cec9a9
jmctune <17505625+jmctune@users.noreply.github.com>
2022-11-08 23:03:58 -0600

    fix version url.

--- app/common/constants.py
+++ app/common/constants.py
@@ -1,5 +1,5 @@
 GITHUB_HEX_DICT_CSV_URL = "https://raw.githubusercontent.com/dqxtranslationproject/dqx-custom-translations/main/csv/hex_dict.csv"
 GITHUB_CUSTOM_TRANSLATIONS_ZIP_URL = "https://github.com/dqxtranslationproject/dqx-custom-translations/archive/refs/heads/main.zip"
-GITHUB_CLARITY_VERSION_UPDATE_URL = "https://raw.githubusercontent.com/dqxtranslationproject/dqxclarity/weblate/version.update"
+GITHUB_CLARITY_VERSION_UPDATE_URL = "https://raw.githubusercontent.com/dqx-translation-project/dqxclarity/main/version.update"
 GITHUB_CLARITY_MERGE_XLSX_URL = "https://raw.githubusercontent.com/dqxtranslationproject/dqx-custom-translations/main/csv/merge.xlsx"
 GITHUB_WEBLATE_ZIP_URL = "https://github.com/dqxtranslationproject/dqx_translations/archive/refs/heads/main.zip"
