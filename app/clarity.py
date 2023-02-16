183af01ea3ad047ed752ecf2323176233ebf2e0e
xshobux <84678540+xshobux@users.noreply.github.com>
2023-02-15 22:05:06 -0500

    maintenance fix

--- app/clarity.py
+++ app/clarity.py
@@ -225,9 +225,9 @@ def scan_for_npc_names():
         if npc_list := pattern_scan(pattern=npc_monster_pattern, return_multiple=True):
             for address in npc_list:
                 npc_type = read_bytes(address + 36, 2)
-                if npc_type == b"\xD4\xE6" or npc_type == b"\x78\xD4":
+                if npc_type == b"\x58\xE4" or npc_type == b"\xFC\xD1":
                     data = "NPC"
-                elif npc_type == b"\xF0\xD6":
+                elif npc_type == b"\x74\xD4":
                     data = "AI_NAME"
                 else:
                     continue
