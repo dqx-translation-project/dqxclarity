8e128f2c0975b69e5479aa575155d9c4f8b58e04
jmctune <17505625+jmctune@users.noreply.github.com>
2023-05-01 20:04:56 -0500

    redo updater to keep zip in memory; remove all non-user data files and dump what's in the zip.

--- app/updater.py
+++ app/updater.py
@@ -1,56 +1,78 @@
-import zipfile
+from io import BytesIO
+import glob
+from zipfile import ZipFile as zip
 import os
 import shutil
-from pathlib import Path
-import requests
-from common.errors import message_box_fatal_error
+import sys
+import subprocess
+from urllib.request import urlopen, Request
+
+CLARITY_URL = "https://github.com/dqxtranslationproject/dqxclarity/releases/latest/download/dqxclarity.zip"
 
 
-os.system("taskkill /f /im DQXClarity.exe >nul 2>&1")
-print("Clarity is updating. Please wait...")
+def process_exists(process_name):
+    # https://stackoverflow.com/a/29275361
+    call = 'TASKLIST', '/FI', 'imagename eq %s' % process_name
+    output = subprocess.check_output(call).decode()
+    last_line = output.strip().split('\r\n')[-1]
+    return last_line.lower().startswith(process_name.lower())
+
+
+def kill_clarity_exe():
+    os.system("taskkill /f /im DQXClarity.exe >nul 2>&1")
+
+
+def download_latest_zip():
+    req = Request(CLARITY_URL)
+    data = urlopen(req, timeout=15)
+    if data.status == 200:
+        zfile = zip(BytesIO(data.read()))
+    return zfile
+
+
+def delete_file(file: str):
+    try:
+        os.remove(file)
+    except:
+        shutil.rmtree(file, ignore_errors=True)
+
+
+if process_exists("DQXGame.exe"):
+    input("Please close DQX before updating. Re-launch dqxclarity once the game has been closed.\n\nPress ENTER to close this window.")
+    sys.exit()
+
+print("dqxclarity is updating. Please wait...")
 
 try:
-    shutil.rmtree("update_temp", ignore_errors=True)
+    z_data = download_latest_zip()
 except Exception as e:
-    pass
+    input(f"Failed to download the latest update. Please try again or download the update manually from Github.\n\nError: {e}")
+    sys.exit()
 
-try:
-    URL = "https://github.com/dqxtranslationproject/dqxclarity/releases/latest/download/dqxclarity.zip"
-    r = requests.get(URL, timeout=15)
-except Exception as e:
-    message_box_fatal_error(
-        "Timeout",
-        "Timed out trying to download latest update. Please try again or download the update manually from Github.",
-    )
+# don't remove user's preferences
+files_to_ignore = [
+    "clarity_dialog.db",
+    "user_settings.ini",
+    "defaults.pref",
+]
 
-with open("dqxclarity.zip", "wb") as weblate_zip:
-    weblate_zip.write(r.content)
+clarity_path = os.path.split(__file__)[0]
+clarity_files = glob.glob(f"{clarity_path}/*")
+for file in clarity_files:
+    basename = os.path.basename(file)
+    if basename in files_to_ignore:
+        continue
+    delete_file(file)
 
-with zipfile.ZipFile("dqxclarity.zip", "r") as zipObj:
-    Path("update_temp/dqxclarity/misc_files/clarity_dialog.db").unlink()
-    Path("update_temp/dqxclarity/misc_files/python39.dll").unlink()
-    Path("update_temp/dqxclarity/user_settings.ini").unlink()
+for obj in z_data.infolist():
+    basename = os.path.basename(obj.filename)
+    if basename in files_to_ignore:
+        continue
+    obj.filename = obj.filename.replace("dqxclarity/", "")
+    if obj.filename:
+        z_data.extract(obj, ".")
 
-Path("dqxclarity.zip").unlink()
-UPDATE_FILE_PATH = "update_temp/dqxclarity"
-update_files = os.listdir(UPDATE_FILE_PATH)
+# remove venv so we can re-install any new modules if we introduce or bump new ones.
+delete_file("venv")
 
-for file in update_files:
-    full_file_name = os.path.join(UPDATE_FILE_PATH, file)
-    if os.path.isdir(full_file_name):
-        sub_folder_files = os.listdir(full_file_name)
-        for subfile in sub_folder_files:
-            full_subfile_name = os.path.join(full_file_name, subfile)
-            if full_subfile_name != "update_temp/dqxclarity\\pymem\\ressources":
-                try:
-                    shutil.copy(full_subfile_name, os.path.join(os.getcwd(), file))
-                except shutil.SameFileError:
-                    pass
-    else:
-        try:
-            shutil.copy(full_file_name, os.getcwd())
-        except shutil.SameFileError:
-            pass
-
-shutil.rmtree("update_temp", ignore_errors=True)
-message_box_fatal_error("Success", "Update complete. Please press OK and relaunch Clarity.")
+input("Success. Please re-launch dqxclarity. Press ENTER to close this window.")
