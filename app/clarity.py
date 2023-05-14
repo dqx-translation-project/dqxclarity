14c60df1bb399882524e74ca78778a2adb505dd7
jmctune <17505625+jmctune@users.noreply.github.com>
2023-05-14 00:07:52 -0500

    Strip out try/except and try catching in the loop at the bottom. Look for 299's and ignore them if seen.

--- app/clarity.py
+++ app/clarity.py
@@ -1,6 +1,8 @@
 import sys
 import time
 from loguru import logger
+import pymem
+
 from common.translate import (
     sqlite_read,
     sqlite_write,
@@ -35,25 +37,23 @@ def scan_for_player_names():
     Scans for addresses that are related to a specific
     pattern to translate player names.
     """
-    try:
-        if player_list := pattern_scan(pattern=player_name_pattern, return_multiple=True):
-            for address in player_list:
-                player_name_address = address + 48  # len of num of (player_name_pattern - 1)
-                try:
-                    ja_player_name = read_string(player_name_address)
-                    romaji_name = convert_into_eng(ja_player_name)
-                    if romaji_name != ja_player_name:
-                        write_bytes(player_name_address, b"\x04" + romaji_name.encode("utf-8") + b"\x00")
-                except UnicodeDecodeError:
+    if player_list := pattern_scan(pattern=player_name_pattern, return_multiple=True):
+        for address in player_list:
+            player_name_address = address + 48  # len of num of (player_name_pattern - 1)
+            try:
+                ja_player_name = read_string(player_name_address)
+                romaji_name = convert_into_eng(ja_player_name)
+                if romaji_name != ja_player_name:
+                    write_bytes(player_name_address, b"\x04" + romaji_name.encode("utf-8") + b"\x00")
+            except UnicodeDecodeError:
+                continue
+            except pymem.exception.WinAPIError as e:
+                if "error_code: 299" in str(e):  # impartial read, just ignore.
                     continue
-                except Exception as e:
-                    logger.warning("INFO ONLY: Failed to write player name.")
-    except TypeError:
-        logger.error(f"Cannot find DQX process. Must have closed? Exiting.")
-        sys.exit()
-    except Exception as e:
-        logger.error(f"Cannot find DQX process. Must have closed? Exiting.\nError: {e}")
-        sys.exit()
+                else:
+                    raise
+            except Exception as e:
+                logger.debug(f"Failed to write player name {romaji_name}.")
 
 
 def scan_for_sibling_names():
@@ -61,45 +61,31 @@ def scan_for_sibling_names():
     Scans for addresses that are related to a specific
     pattern to translate sibling names.
     """
-    try:
-        if sibling_list := pattern_scan(pattern=sibling_name_pattern, return_multiple=True):
-            for address in sibling_list:
-                sibling_name_address = address + 51  # len of num of (sibling_name_pattern - 1)
-                try:
-                    ja_sibling_name = read_string(sibling_name_address)
-                    romaji_name = convert_into_eng(ja_sibling_name)
-                    if romaji_name != ja_sibling_name:
-                        write_bytes(sibling_name_address, b"\x04" + romaji_name.encode("utf-8") + b"\x00")
-                except UnicodeDecodeError:
-                    continue
-                except Exception as e:
-                    logger.warning("INFO ONLY: Failed to write sibling name.")
-    except TypeError:
-        logger.error(f"Cannot find DQX process. Must have closed? Exiting.")
-        sys.exit()
-    except Exception as e:
-        logger.error(f"Cannot find DQX process. Must have closed? Exiting.\nError: {e}")
-        sys.exit()
-        
+    if sibling_list := pattern_scan(pattern=sibling_name_pattern, return_multiple=True):
+        for address in sibling_list:
+            sibling_name_address = address + 51  # len of num of (sibling_name_pattern - 1)
+            try:
+                ja_sibling_name = read_string(sibling_name_address)
+                romaji_name = convert_into_eng(ja_sibling_name)
+                if romaji_name != ja_sibling_name:
+                    write_bytes(sibling_name_address, b"\x04" + romaji_name.encode("utf-8") + b"\x00")
+            except UnicodeDecodeError:
+                continue
+            except Exception as e:
+                logger.debug("Failed to write sibling name.")
+
         
 def scan_for_concierge_names():
-    try:
-        if concierge_names := pattern_scan(pattern=concierge_name_pattern, return_multiple=True):
-            for address in concierge_names:
-                name_addr = address + 12  # jump to name
-                try:
-                    ja_name = read_string(name_addr)
-                    en_name = convert_into_eng(ja_name)
-                    if en_name != ja_name:
-                        write_bytes(name_addr, b"\x04" + str.encode(en_name) + b"\x00")
-                except UnicodeDecodeError:
-                    pass
-    except TypeError:
-        logger.error(f"Cannot find DQX process. Must have closed? Exiting.")
-        sys.exit()
-    except Exception as e:
-        logger.error(f"Error: {e}")
-        sys.exit()
+    if concierge_names := pattern_scan(pattern=concierge_name_pattern, return_multiple=True):
+        for address in concierge_names:
+            name_addr = address + 12  # jump to name
+            try:
+                ja_name = read_string(name_addr)
+                en_name = convert_into_eng(ja_name)
+                if en_name != ja_name:
+                    write_bytes(name_addr, b"\x04" + str.encode(en_name) + b"\x00")
+            except UnicodeDecodeError:
+                pass
 
 
 def scan_for_npc_names():
@@ -114,67 +100,51 @@ def scan_for_npc_names():
         f"{misc_files}/custom_npc_names.json"
     ])
 
-    try:
-        if npc_list := pattern_scan(pattern=npc_monster_pattern, return_multiple=True):
-            for address in npc_list:
-                npc_type = read_bytes(address + 36, 2)
-                if npc_type == b"\x34\xE5" or npc_type == b"\x7C\xD2":
-                    data = "NPC"
-                elif npc_type == b"\xE4\xD4":
-                    data = "AI_NAME"
-                else:
-                    continue
+    if npc_list := pattern_scan(pattern=npc_monster_pattern, return_multiple=True):
+        for address in npc_list:
+            npc_type = read_bytes(address + 36, 2)
+            if npc_type == b"\x34\xE5" or npc_type == b"\x7C\xD2":
+                data = "NPC"
+            elif npc_type == b"\xE4\xD4":
+                data = "AI_NAME"
+            else:
+                continue
 
-                name_addr = address + 48  # jump to name
-                name = read_string(name_addr)
+            name_addr = address + 48  # jump to name
+            name = read_string(name_addr)
 
-                if data == "NPC":
-                    if name in translated_names:
-                        value = translated_names.get(name)
-                        if value:
-                            try:
-                                write_string(name_addr, value)
-                            except Exception as e:
-                                logger.warning(f"Failed to write {data} name {value}.")
-                elif data == "AI_NAME":
-                    en_name = convert_into_eng(name)
-                    if en_name != name:
+            if data == "NPC":
+                if name in translated_names:
+                    value = translated_names.get(name)
+                    if value:
                         try:
-                            write_bytes(name_addr, b"\x04" + en_name.encode("utf-8") + b"\x00")
+                            write_string(name_addr, value)
                         except Exception as e:
-                            logger.warning(f"Failed to write {data} for {en_name}.")
-    except TypeError:
-        logger.error(f"Cannot find DQX process. Must have closed? Exiting.")
-        sys.exit()
-    except Exception as e:
-        logger.error(f"Cannot find DQX process. Must have closed? Exiting.\nError: {e}")
-        sys.exit()
+                            logger.warning(f"Failed to write {data} name {value}.")
+            elif data == "AI_NAME":
+                en_name = convert_into_eng(name)
+                if en_name != name:
+                    try:
+                        write_bytes(name_addr, b"\x04" + en_name.encode("utf-8") + b"\x00")
+                    except Exception as e:
+                        logger.warning(f"Failed to write {data} for {en_name}.")
 
 
 def scan_for_menu_ai_names():
     """
     Scans for the walkthrough address and translates when found, then translates party members.
     """
-    try:
-        if ai_list := pattern_scan(pattern=menu_ai_name_pattern, return_multiple=True):
-            for address in ai_list:
-                ai_name_address = address + 57
-                if ja_ai_name := read_string(ai_name_address):
-                    romaji_name = convert_into_eng(ja_ai_name)
-                    if romaji_name != ja_ai_name:
-                        try:
-                            write_string(ai_name_address, romaji_name)
-                            logger.debug(f"Wrote player name {romaji_name}.")
-                        except Exception as e:
-                            logger.warning("INFO ONLY: Failed to write Menu AI name.")
-    except UnicodeDecodeError:
-        pass
-    except TypeError:
-        logger.error(f"Cannot find DQX process. Must have closed? Exiting.")
-        sys.exit()
-    except Exception as e:
-        logger.error(f"Cannot find DQX process. Must have closed? Exiting.\nError {e}")
-        sys.exit()
+    if ai_list := pattern_scan(pattern=menu_ai_name_pattern, return_multiple=True):
+        for address in ai_list:
+            ai_name_address = address + 57
+            if ja_ai_name := read_string(ai_name_address):
+                romaji_name = convert_into_eng(ja_ai_name)
+                if romaji_name != ja_ai_name:
+                    try:
+                        write_string(ai_name_address, romaji_name)
+                        logger.debug(f"Wrote player name {romaji_name}.")
+                    except Exception as e:
+                        logger.warning("INFO ONLY: Failed to write Menu AI name.")
 
 
 def loop_scan_for_walkthrough():
@@ -185,42 +155,35 @@ def loop_scan_for_walkthrough():
     logger.info("Will watch for walkthrough text.")
 
     while True:
-        try:
-            if address := pattern_scan(pattern=walkthrough_pattern):
-                prev_text = ""
-                while True:
-                    if text := read_string(address + 16):
-                        if text != prev_text:
-                            prev_text = text
-                            if detect_lang(text):
-                                result = sqlite_read(text, "en", "walkthrough")
-                                if result:
-                                    write_string(address + 16, result)
-                                else:
-                                    translated_text = sanitized_dialog_translate(
-                                        api_details["TranslateService"],
-                                        text,
-                                        api_details["TranslateKey"],
-                                        api_details["RegionCode"],
-                                        text_width=31,
-                                        max_lines=3,
-                                    )
-                                    try:
-                                        sqlite_write(text, "walkthrough", translated_text, api_details["RegionCode"])
-                                        write_string(address + 16, translated_text)
-                                        logger.debug("Wrote walkthrough.")
-                                    except Exception as e:
-                                        logger.warning("INFO ONLY: Failed to write walkthrough text.")
-                        else:
-                            time.sleep(0.5)
-            else:
-                time.sleep(0.5)
-        except TypeError:
-            logger.error(f"Cannot find DQX process. Must have closed? Exiting.")
-            sys.exit()
-        except Exception as e:
-            logger.error(f"Cannot find DQX process. Must have closed? Exiting.\nError: {e}")
-            sys.exit()
+        if address := pattern_scan(pattern=walkthrough_pattern):
+            prev_text = ""
+            while True:
+                if text := read_string(address + 16):
+                    if text != prev_text:
+                        prev_text = text
+                        if detect_lang(text):
+                            result = sqlite_read(text, "en", "walkthrough")
+                            if result:
+                                write_string(address + 16, result)
+                            else:
+                                translated_text = sanitized_dialog_translate(
+                                    api_details["TranslateService"],
+                                    text,
+                                    api_details["TranslateKey"],
+                                    api_details["RegionCode"],
+                                    text_width=31,
+                                    max_lines=3,
+                                )
+                                try:
+                                    sqlite_write(text, "walkthrough", translated_text, api_details["RegionCode"])
+                                    write_string(address + 16, translated_text)
+                                    logger.debug("Wrote walkthrough.")
+                                except Exception as e:
+                                    logger.warning("INFO ONLY: Failed to write walkthrough text.")
+                    else:
+                        time.sleep(0.5)
+        else:
+            time.sleep(0.5)
 
 
 def run_scans(player_names=True, npc_names=True, communication_window=True, debug=False):
@@ -251,6 +214,15 @@ def run_scans(player_names=True, npc_names=True, communication_window=True, debu
             if npc_names:
                 scan_for_npc_names()
                 scan_for_concierge_names()
+        except pymem.exception.WinAPIError as e:
+            if "error_code: 299" in str(e):  # impartial read, just ignore.
+                print("299 - but continue")
+                continue
+            else:
+                print("crash")
+                raise
+        except UnicodeDecodeError:
+            pass
         except TypeError:
             logger.error(f"Cannot find DQX process. Must have closed? Exiting.")
             sys.exit()
