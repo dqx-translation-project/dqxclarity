280384f8698dbc07c3d823245179717e19b2d36d
jmctune <17505625+jmctune@users.noreply.github.com>
2023-06-23 19:36:15 -0500

    Move the sanitize method from the autotranslator into actual clarity.

--- app/common/translate.py
+++ app/common/translate.py
@@ -35,19 +35,30 @@ class Translate():
                 Translate.glossary = [ x for x in strings.split("\n") if x ]
 
 
-    def deepl(self, text: str):
+    def deepl(self, text: list):
         self.region_code = Translate.region_code
         if self.region_code.lower() == "en":
             self.region_code = "en-us"
         translator = deepl.Translator(Translate.api_key)
-        response = translator.translate_text(text=text, source_lang="ja", target_lang=self.region_code)
-        return response.text
+        response = translator.translate_text(
+            text=text,
+            source_lang="ja",
+            target_lang=self.region_code,
+            preserve_formatting=True
+        )
+        text_results = []
+        for result in response:
+            text_results.append(result.text)
+        return text_results
 
 
-    def google(self, text: str):
+    def google(self, text: list):
         service = build("translate", "v2", developerKey=Translate.api_key)
-        response = service.translations().list(source="ja", target="en", format="text", q=[text]).execute()
-        return response["translations"][0]["translatedText"]
+        response = service.translations().list(source="ja", target="en", format="text", q=text).execute()
+        text_results = []
+        for result in response["translations"]:
+            text_results.append(result["translatedText"])
+        return text_results
 
 
     def __glossify(self, text):
@@ -59,13 +70,287 @@ class Translate():
         return text
 
 
-    def translate(self, text: str):
-        text = self.__glossify(text)
+    def __normalize_text(self, text: str) -> str:
+        """
+        "Normalize" text by only using latin alphabet.
+
+        :param text: Text to normalize
+        :returns: Normalized text.
+        """
+        return unicodedata.normalize("NFKD", text).encode("ascii", "ignore").decode()
+
+
+    def __swap_placeholder_tags(self, text: str, swap_back=False) -> str:
+        if not swap_back:
+            text = text.replace("<pc_hiryu>", "<&13_aaaaaaa>")
+            text = text.replace("<cs_pchero_hiryu>", "<&13_aaaaaab>")
+            text = text.replace("<cs_pchero_race>", "<&8_aaa>")
+            text = text.replace("<cs_pchero>", "<13_aaaaaac>")
+            text = text.replace("<kyodai_rel1>", "<&7_aa>")
+            text = text.replace("<kyodai_rel2>", "<&7_ab>")
+            text = text.replace("<kyodai_rel3>", "<&7_ac>")
+            text = text.replace("<pc_hometown>", "<&8_aab>")
+            text = text.replace("<pc_race>", "<&8_aac>")
+            text = text.replace("<pc_rel1>", "<&7_ad>")
+            text = text.replace("<pc_rel2>", "<&7_ae>")
+            text = text.replace("<pc_rel3>", "<&7_af")
+            text = text.replace("<kyodai>", "<&13_aaaaaac>")
+            text = text.replace("<pc>", "<&13_aaaaaad>")
+            text = text.replace("<client_pcname>", "<&13_aaaaaae>")
+            text = text.replace("<heart>", "<&2a>")
+            text = text.replace("<diamond>", "<&2b>")
+            text = text.replace("<spade>", "<&2c>")
+            text = text.replace("<clover>", "<&2d>")
+            text = text.replace("<r_triangle>", "<&2e>")
+            text = text.replace("<l_triangle>", "<&2f>")
+            text = text.replace("<half_star>", "<&2g>")
+            text = text.replace("<null_star>", "<&2h>")
+            text = text.replace("<npc>", "<&13_aaaaaaf>")
+            text = text.replace("<pc_syokugyo>", "<&13_aaaaaag>")
+            text = text.replace("<pc_original>", "<&13_aaaaaah>")
+            text = text.replace("<log_pc>", "<&13_aaaaaai>")
+            text = text.replace("<1st_title>", "<&20_aaaaaaaaaaaaaa>")
+            text = text.replace("<2nd_title>", "<&20_aaaaaaaaaaaaab>")
+            text = text.replace("<3rd_title>", "<&20_aaaaaaaaaaaaac>")
+            text = text.replace("<4th_title>", "<&20_aaaaaaaaaaaaad>")
+            text = text.replace("<5th_title>", "<&20_aaaaaaaaaaaaae>")
+            text = text.replace("<6th_title>", "<&20_aaaaaaaaaaaaaf>")
+            text = text.replace("<7th_title>", "<&20_aaaaaaaaaaaaag>")
+        else:
+            text = text.replace("<&13_aaaaaaa>", "<pc_hiryu>")
+            text = text.replace("<&13_aaaaaab>", "<cs_pchero_hiryu>")
+            text = text.replace("<&8_aaa>", "<cs_pchero_race>")
+            text = text.replace("<13_aaaaaac>", "<cs_pchero>")
+            text = text.replace("<&7_aa>", "<kyodai_rel1>")
+            text = text.replace("<&7_ab>", "<kyodai_rel2>")
+            text = text.replace("<&7_ac>", "<kyodai_rel3>")
+            text = text.replace("<&8_aab>", "<pc_hometown>")
+            text = text.replace("<&8_aac>", "<pc_race>")
+            text = text.replace("<&7_ad>", "<pc_rel1>")
+            text = text.replace("<&7_ae>", "<pc_rel2>")
+            text = text.replace("<&7_af", "<pc_rel3>")
+            text = text.replace("<&13_aaaaaac>", "<kyodai>")
+            text = text.replace("<&13_aaaaaad>", "<pc>")
+            text = text.replace("<&13_aaaaaae>", "<client_pcname>")
+            text = text.replace("<&2a>", "<heart>")
+            text = text.replace("<&2b>", "<diamond>")
+            text = text.replace("<&2c>", "<spade>")
+            text = text.replace("<&2d>", "<clover>")
+            text = text.replace("<&2e>", "<r_triangle>")
+            text = text.replace("<&2f>", "<l_triangle>")
+            text = text.replace("<&2g>", "<half_star>")
+            text = text.replace("<&2h>", "<null_star>")
+            text = text.replace("<&13_aaaaaaf>", "<npc>")
+            text = text.replace("<&13_aaaaaag>", "<pc_syokugyo>")
+            text = text.replace("<&13_aaaaaah>", "<pc_original>")
+            text = text.replace("<&13_aaaaaai>", "<log_pc>")
+            text = text.replace("<&20_aaaaaaaaaaaaaa>", "<1st_title>")
+            text = text.replace("<&20_aaaaaaaaaaaaab>", "<2nd_title>")
+            text = text.replace("<&20_aaaaaaaaaaaaac>", "<3rd_title>")
+            text = text.replace("<&20_aaaaaaaaaaaaad>", "<4th_title>")
+            text = text.replace("<&20_aaaaaaaaaaaaae>", "<5th_title>")
+            text = text.replace("<&20_aaaaaaaaaaaaaf>", "<6th_title>")
+            text = text.replace("<&20_aaaaaaaaaaaaag>", "<7th_title>")
+
+        return text
+
+
+    def __wrap_text(self, text: str, width: int, max_lines=None) -> str:
+        """
+        Wrap text to n characters per line.
+        """
+        return textwrap.fill(text, width=width, max_lines=max_lines, replace_whitespace=False)
+
+
+    def __add_line_endings(self, text: str) -> str:
+        """
+        Adds <br> flags every 3 lines to a string. Used to break up the
+        text in a dialog window.
+
+        :param text: Text to add the <br> tags to.
+        :returns: A new string with the text broken up by <br> tags.
+        """
+        count_list = [ i for i in range(3, 500, 4) ] # 500 is arbitrary, but we should never hit this.
+        split_text = text.split("\n")
+        try:
+            for i in count_list:
+                _ = split_text[i]
+                split_text.insert(i, "<br>")
+        except IndexError:
+            split_text = [ x for x in split_text if x ]
+            output = "\n".join(split_text)
+            return output
+
+
+    def translate(self, text: list):
+        """
+        Translates a list of strings, passing them through our glossary first.
+
+        :param text: List of text strings to be translated.
+        :returns: A translated list of strings in the same order they were given.
+        """
+        count = 0
+        for i in text:
+            text[count] = self.__glossify(i)
+            count += 1
         if Translate.service == "deepl":
             return self.deepl(text)
         if Translate.service == "google":
             return self.google(text)
         return None
+
+
+    def sanitize_and_translate(self, text: str, wrap_width: int, max_lines=None, add_brs=True):
+        """
+        Sanitizes different tags and symbols, then translates the string.
+
+        :param text: String to be translated.
+        :param wrap_width: How many characters the returning string should contain per line.
+        :param max_lines: The maximum amount of lines to return. Extra lines are truncated with "..."
+        :param add_brs: Whether to inject "<br>" every three lines to break up text. Used for dialog mainly.
+        """
+        # manage our own line endings later
+        output = text.replace("<br>", "　")
+
+        # remove any tag alignments
+        alignments = ["<center>", "<right>", "<left>"]
+        for alignment in alignments:
+            output = output.replace(alignment, "")
+
+        # remove any other oddities that don't look great in english
+        oddities = ["「", "…"]
+        for oddity in oddities:
+            output = output.replace(oddity, "")
+
+        # remove the full width space that starts on a new line
+        output = output.replace("\n　", "　")
+
+        # replace any <color*> tags with & as they are part of the string
+        output = output.replace("<color_", "<&color_")
+
+        name_tags = ["<pc>", "<cs_pchero>", "<kyodai>"]
+
+        # removes all of the honorifics added at the end of the tags
+        honorifics = ["さま", "君", "どの", "ちゃん", "くん", "様", "さーん", "殿", "さん",]
+        for tag in name_tags:
+            for honorific in honorifics:
+                output = output.replace(f"{tag}{honorific}", tag)
+
+        # replace all variable name tags that expand to other text
+        output = self.__swap_placeholder_tags(output)
+
+        # pass string through our glossary to replace any common words
+        output = self.__glossify(output)
+
+        # re-assign this string. this is now our "pristine" string we'll be using later.
+        pristine_str = output
+
+        # get the text to translate, splitting on all tags that don't start with % or &
+        tag_re = re.compile("(<[^%&]*?>)")
+        select_re = re.compile(r"(<select.*>)")
+        str_split = [ x for x in re.split(tag_re, output) if x ]
+
+        count = 0
+        str_attrs = {}
+
+        # iterate over each string, handling based on condition
+        for str in str_split:
+            if not re.match(tag_re, str):
+
+                # sole new lines need to stay where they are.
+                if str == "\n":
+                    continue
+
+                # capture position of the string and replace with placeholder text
+                pristine_str = pristine_str.replace(str, f"<replace_me_index_{count}>")
+
+                # <select*> lists always start with their first entry being a newline.
+                # if we see this, look back one index to see if we're inside a select tag.
+                if str.startswith("\n"):
+                    lookback = str_split.index(str) - 1
+                    if re.match(select_re, str_split[lookback]):
+                        str_attrs[count] = {
+                            "text": str,
+                            "is_list": True,
+                            "prepend_newline": False,
+                            "append_newline": False,
+                        }
+                        count += 1
+                        continue
+
+                # capture how the newline was originally placed
+                append_newline = False
+                if str.endswith("\n"):
+                    append_newline = True
+
+                prepend_newline = False
+                if str.startswith("\n"):
+                    prepend_newline = True
+
+                str = str.replace("\n", "")
+
+                str_attrs[count] = {
+                    "text": str,
+                    "is_list": False,
+                    "prepend_newline": prepend_newline,
+                    "append_newline": append_newline,
+                }
+
+                count += 1
+
+        # translate our list of strings
+        to_translate = []
+        count = 0
+        for str in str_attrs:
+            to_translate.append(str_attrs[count]["text"])
+            count += 1
+        translated_list = self.translate(text=to_translate)
+
+        # update our str_attrs dict with the new, translated string
+        count = 0
+        for str in translated_list:
+            str_attrs[count]["text"] = str
+            count += 1
+
+        count = 0
+        # search for any weird space usage and remove it.
+        # this comes from deepl and are all scenarios that have been seen with
+        # translations coming back from machine translation.
+        for _ in str_attrs:
+            str_text = str_attrs[count]["text"]
+            str_text = str_text.replace("　 ", " ")
+            str_text = str_text.replace("　", " ")
+            str_text = str_text.replace("  ", "")
+
+            updated_str = self.__normalize_text(str_text)
+            updated_str = updated_str.replace("<&color_", "<color_")  # put our color tag back.
+
+            if str_attrs[count]["is_list"]:
+                # select lists will always have more than 1 entry..
+                # leave selection lists alone. please don't fuck this up, deepl
+                updated_str = self.__swap_placeholder_tags(updated_str, swap_back=True)
+
+                # deepl occasionally indents our list lines.. even though they weren't originally indented
+                updated_str = updated_str.replace("\n ", "\n")
+                pristine_str = pristine_str.replace(f"<replace_me_index_{count}>", updated_str)
+
+            else:
+                # wrap the text and inject <br>'s to break the text up
+                updated_str = self.__wrap_text(updated_str, width=wrap_width, max_lines=max_lines)
+                updated_str = self.__swap_placeholder_tags(updated_str, swap_back=True)
+
+                if add_brs:
+                    updated_str = self.__add_line_endings(updated_str)
+                if str_attrs[count]["prepend_newline"]:
+                    updated_str = "\n" + updated_str
+                if str_attrs[count]["append_newline"]:
+                    updated_str += "\n"
+
+                pristine_str = pristine_str.replace(f"<replace_me_index_{count}>", updated_str)
+
+            count += 1
+
+        return pristine_str
 
 
 def sanitized_dialog_translate(dialog_text, text_width=45, max_lines=None) -> str:
