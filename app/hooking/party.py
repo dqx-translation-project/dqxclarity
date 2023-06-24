74073c0d995371754f4adecf5ed1967df516b727
jmctune <17505625+jmctune@users.noreply.github.com>
2023-06-23 19:46:18 -0500

    this shellcode doesn't work, but clean it up anyways.

--- app/hooking/party.py
+++ app/hooking/party.py
@@ -4,7 +4,7 @@ from json import dumps
 
 
 # this works, but we can't use it due to integrity checks.
-def rename_party_members_shellcode(ebx_address: int, debug: bool) -> str:
+def rename_party_members_shellcode(ebx_address: int) -> str:
     """
     Returns shellcode to rename party members into Romaji.
     ebx_address: Where text can be modified
@@ -25,11 +25,8 @@ try:
     og_working_dir = getcwd()
     chdir(working_dir)
 
-    from common.lib import setup_logger
     from common.memory import read_string, write_string, unpack_to_int
     from common.translate import convert_into_eng
-
-    logger = setup_logger('out', 'out.log')
 
     player_addr = unpack_to_int({ebx_address})
     player_addr_name = player_addr + 64
@@ -37,7 +34,6 @@ try:
 
     en_name = convert_into_eng(player_name)
     write_string(player_addr_name, en_name)
-    logger.info('Wrote player name ' + str(en_name))
     chdir(og_working_dir)
 except Exception as e:
     with open('out.log', 'a+') as f:
