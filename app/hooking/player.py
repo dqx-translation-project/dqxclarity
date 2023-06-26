4cfb976df61cbd45aed28de45cbc57a11f7a2fa1
jmctune <17505625+jmctune@users.noreply.github.com>
2023-06-25 21:54:12 -0500

    check specific byte to determine your sibling's relationship to the player. left some notes at the bottom until we've implemented placeholder string swapping.

--- app/hooking/player.py
+++ app/hooking/player.py
@@ -3,7 +3,7 @@ import os
 import sqlite3
 import sys
 from common.lib import get_abs_path
-from common.memory import unpack_to_int, read_string
+from common.memory import unpack_to_int, read_string, read_bytes
 
 class GetPlayer(object):
 
@@ -15,11 +15,27 @@ class GetPlayer(object):
 
         player = read_string(self.address + 24)
         sibling = read_string(self.address + 96)
+        sibling_relationship = self.__determine_sibling_relationship()
 
-        self.__write_db(player_name=player, sibling_name=sibling)
+        self.__write_db(
+            player_name=player,
+            sibling_name=sibling,
+            sibling_relationship=sibling_relationship
+        )
+
+    def __determine_sibling_relationship(self):
+        check_byte = read_bytes(self.address + 96 + 19, size=1)
+        if check_byte == b"\x01":
+            return "older_brother"
+        if check_byte == b"\x02":
+            return "younger_brother"
+        if check_byte == b"\x03":
+            return "older_sister"
+        if check_byte == b"\x04":
+            return "younger_sister"
 
 
-    def __write_db(self, player_name: str, sibling_name: str):
+    def __write_db(self, player_name: str, sibling_name: str, sibling_relationship: str):
         db_file = "/".join([get_abs_path(__file__), "../misc_files/clarity_dialog.db"])
 
         query = f"""
@@ -27,7 +43,8 @@ class GetPlayer(object):
         DELETE FROM player;
         INSERT INTO player (type, name) VALUES
             ('player', '{player_name}'),
-            ('sibling', '{sibling_name}');
+            ('sibling', '{sibling_name}'),
+            ('sibling_relationship', '{sibling_relationship}');
         END TRANSACTION;
         """
 
@@ -56,3 +73,21 @@ except Exception as e:
     """
 
     return str(shellcode)
+
+# 115 bytes - <kyodai_rel*> byte
+# 01 - older brother
+#     <kyodai_rel1>: 兄ちゃん
+#     <kyodai_rel2>: お兄ちゃん
+#     <kyodai_rel3>: 兄
+# 02 - younger brother
+#     <kyodai_rel1>: 弟
+#     <kyodai_rel2>: 弟
+#     <kyodai_rel3>: 弟
+# 03 - older sister
+#     <kyodai_rel1>: 姉ちゃん
+#     <kyodai_rel2>: お姉ちゃん
+#     <kyodai_rel3>: 姉
+# 04 - younger sister
+#     <kyodai_rel1>: 妹
+#     <kyodai_rel2>: 妹
+#     <kyodai_rel3>: 妹
