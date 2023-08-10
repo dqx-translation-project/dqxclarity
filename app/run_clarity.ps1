eae58407d30a39831e2d6127ac55bb2e2fbbf275
Serany <17505625+jmctune@users.noreply.github.com>
2023-08-09 19:18:06 -0500

    Update to support installing tkinter, as well as refactor the script for better checking.

--- app/run_clarity.ps1
+++ app/run_clarity.ps1
@@ -20,6 +20,47 @@ function RemoveFile($path) {
     }
 }
 
+function PromptForInputAndExit() {
+    Read-Host "Press ENTER to close"
+    Exit
+}
+
+function DownloadAndInstallPython() {
+    $ProgressPreference = "SilentlyContinue"  # workaround to faster download speeds using IWR
+    LogWrite "Downloading Python executable from the internet."
+    Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.11.3/python-3.11.3.exe -OutFile python-3.11.3.exe
+
+    $PythonMD5 = Get-FileHash .\python-3.11.3.exe -Algorithm MD5
+    if ($PythonMD5.Hash -ne "691232496E346CE0860AEF052DD6844F") {
+        LogWrite "File download did not complete successfully. Please re-run this script and try again. $HelpMessage"
+        RemoveFile "python-3.11.3.exe"
+        Read-Host "Press ENTER to close."
+        Exit
+    }
+
+    LogWrite "Launching Python 3.11 installer and installing Python for you. Please wait."
+    .\python-3.11.3.exe /passive InstallAllUsers=1 PrependPath=1 Include_doc=0 Include_tcltk=1 Include_test=0 Shortcuts=0 SimpleInstallDescription="Installing necessary components for dqxclarity." | Out-Null
+    $PythonInstallPath = PythonExePath
+
+    if (!$PythonInstallPath) {
+        LogWrite "Failed to install Python. Please try again. $HelpMessage"
+        Read-Host "Press ENTER to close."
+        Exit
+    }
+}
+
+function VerifyPythonVenv() {
+    try {
+		& .\venv\Scripts\activate
+        & .\venv\Scripts\python.exe -c "import click"
+    }
+    catch {
+        LogWrite "Virtual environment did not install correctly. Re-open dqxclarity to try again. $HelpMessage"
+        RemoveFile "venv"
+        PromptForInputAndExit
+    }
+}
+
 $ErrorActionPreference="SilentlyContinue"
 Stop-Transcript | Out-Null
 $ErrorActionPreference = "Continue"
@@ -29,6 +70,7 @@ $HelpMessage = "If you need help, please join the DQX Discord and post your ques
 
 $PythonInstallPath = PythonExePath
 
+# install Python if missing
 if (!$PythonInstallPath) {
     LogWrite "Could not find Python installation for Python 3.11-32."
 
@@ -36,26 +78,8 @@ if (!$PythonInstallPath) {
     $Result = $Shell.popup("Could not find Python 3.11 installation. Do you want to install it?",0,"Question",4+32)
 
     if ($Result -eq 6) {
-        $ProgressPreference = "SilentlyContinue"  # workaround to faster download speeds using IWR
-        LogWrite "Downloading Python executable from the internet."
-        Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.11.3/python-3.11.3.exe -OutFile python-3.11.3.exe
-        $PythonMD5 = Get-FileHash .\python-3.11.3.exe -Algorithm MD5
-        if ($PythonMD5.Hash -ne "691232496E346CE0860AEF052DD6844F") {
-            LogWrite "File download did not complete successfully. Please re-run this script and try again. $HelpMessage"
-            RemoveFile "python-3.11.3.exe"
-            Read-Host "Press ENTER to close."
-            Exit
-        } else {
-            LogWrite "Launching Python 3.11 installer and installing Python for you. Please wait."
-            .\python-3.11.3.exe /passive InstallAllUsers=1 PrependPath=1 Include_doc=0 Include_tcltk=1 Include_test=0 Shortcuts=0 SimpleInstallDescription="Installing necessary components for dqxclarity." | Out-Null
-            $PythonInstallPath = PythonExePath
-
-            if (!$PythonInstallPath) {
-                LogWrite "Failed to install Python. Please try again. $HelpMessage"
-                Read-Host "Press ENTER to close."
-                Exit
-            }
-        }
+        Write-Host "test"
+        #DownloadAndInstallPython
     } else {
         LogWrite "You selected 'No'. Python 3.11 is required to use dqxclarity. Exiting."
         Read-Host "Press ENTER to close."
@@ -63,64 +87,71 @@ if (!$PythonInstallPath) {
     }
 }
 
-if (Test-Path -Path "venv") { # check to see if tkinter is installed
-	try {
-		& .\venv\Scripts\activate
-		& .\venv\Scripts\python.exe -c "import tkinter"
-	}
-	catch {
-		$Result = $Shell.popup("Python must be updated to install a new feature Clarity requires. Install now?",0,"Question",4+32)
-		if ($Result -eq 6) {
-			$ProgressPreference = "SilentlyContinue"  # workaround to faster download speeds using IWR
-			LogWrite "Downloading Python executable from the internet."
-			Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.11.3/python-3.11.3.exe -OutFile python-3.11.3.exe
-			$PythonMD5 = Get-FileHash .\python-3.11.3.exe -Algorithm MD5
-			if ($PythonMD5.Hash -ne "691232496E346CE0860AEF052DD6844F") {
-				LogWrite "File download did not complete successfully. Please re-run this script and try again. $HelpMessage"
-				RemoveFile "python-3.11.3.exe"
-				Read-Host "Press ENTER to close."
-				Exit
-			} else {
-				LogWrite "Launching Python 3.11 installer and installing Python for you. Please wait."
-				.\python-3.11.3.exe /passive InstallAllUsers=1 PrependPath=1 Include_doc=0 Include_tcltk=1 Include_test=0 Shortcuts=0 SimpleInstallDescription="Installing necessary components for dqxclarity." | Out-Null
-				$PythonInstallPath = PythonExePath
-
-				if (!$PythonInstallPath) {
-					LogWrite "Failed to install Python. Please try again. $HelpMessage"
-					Read-Host "Press ENTER to close."
-					Exit
-				}
-			}
-		} else {
-			LogWrite "You selected 'No'. Clarity will not function correctly without this installation. Exiting."
-			Read-Host "Press ENTER to close."
-			Exit
-		}
-		RemoveFile "venv"
-		Read-Host "Press ENTER to close"
-		Exit
-	}
-}
-
-if (Test-Path -Path "venv") {
+# check if the user already has a virtual environment folder
+if (-not (Test-Path -Path "venv")) {
     try {
-		& .\venv\Scripts\activate
-        & .\venv\Scripts\python.exe -c "import click"
+        LogWrite "Creating virtual environment."
+        & $PythonInstallPath -m venv venv
     }
     catch {
-        LogWrite "Virtual environment did not install correctly. Re-open dqxclarity to try again. $HelpMessage"
+        LogWrite "An error occurred during virtual environment initialization. Please try again. $HelpMessage"
+        $_
         RemoveFile "venv"
-        Read-Host "Press ENTER to close"
-        Exit
+        PromotForInputAndExit
     }
-} else {
-    LogWrite "Creating virtual environment."
-    & $PythonInstallPath -m venv venv
-	& .\venv\Scripts\activate
+}
+
+# try to activate the found virtual environment
+try {
+    & .\venv\Scripts\activate
+}
+catch {
+    LogWrite "Could not activate virtual environment. Please try again. $HelpMessage"
+    $_
+    RemoveFile "venv"
+    PromotForInputAndExit
+}
+
+# install tkinter if it's missing.
+# we use to automate installations without tkinter enabled, so we now need to check
+# if it's installed. when we upgrade Python versions again, we can remove this block
+# and install tkinter by default.
+& .\venv\Scripts\python.exe -c "import tkinter" 2> $null
+if ($? -eq $False) {
+    $Shell = New-Object -comobject "WScript.Shell"
+    $Result = $Shell.popup("Python must be updated to support a new feature Clarity requires. Install now?",0,"Question",4+32)
+    if ($Result -eq 6) {
+        DownloadAndInstallPython
+        Write-Host "Clarity must be restarted to use the new changes. Please close Clarity and relaunch."
+        RemoveFile "venv"
+        PromptForInputAndExit
+    } else {
+        LogWrite "You selected 'No'. Clarity will not function correctly without this installation. Exiting."
+        PromptForInputAndExit
+    }
+}
+
+# install python dependencies
+try {
     LogWrite "Updating pip and installation dependencies."
     & .\venv\Scripts\python.exe -m pip install --upgrade pip setuptools wheel --quiet
     LogWrite "Installing dqxclarity dependencies."
     & .\venv\Scripts\pip.exe install -r requirements.txt --quiet
+}
+catch {
+    LogWrite "An error occurred during virtual environment initialization. Please try again. $HelpMessage"
+    $_
+    RemoveFile "venv"
+    PromotForInputAndExit
+}
+
+# verify dependencies installed correctly by attempting to import something that was installed.
+& .\venv\Scripts\python.exe -c "import click" 2> $null
+if ($? -eq $False) {
+    DownloadAndInstallPython
+    LogWrite "An error occurred while verifying dependency installation. Please try again. $HelpMessage"
+    RemoveFile "venv"
+    PromptForInputAndExit
 }
 
 LogWrite "Python install location: $PythonInstallPath"
