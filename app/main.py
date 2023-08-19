bda6633e9b4d2f12497b4fe05cbc85bceaa788e1
Serany <17505625+jmctune@users.noreply.github.com>
2023-08-19 10:54:54 -0500

    Update gitignore

--- app/main.py
+++ app/main.py
@@ -15,8 +15,7 @@ from dqxcrypt.dqxcrypt import start_logger
 @click.option('-p', '--player-names', is_flag=True,help="Scans for player names and changes them to their Romaji counterpart.")
 @click.option('-n', '--npc-names', is_flag=True, help="Scans for NPC names and changes them to their Romaji counterpart.")
 @click.option('-l', '--community-logging', is_flag=True, help="Enables dumping important game information that the dqxclarity devs need to continue this project.")
-@click.option('-z', '--disable-translations', is_flag=True, help="Only runs initialization of dqxclarity, which checks for updates and validity of your user_settings.ini file.")
-@click.option('-d', '--update-dat', is_flag=True, help="Update the translated idx and dat file with the latest from Github. Requires the game to be closed. When toggled, this will only update the dat/idx. No other arguments will be processed. If you intend to fully run dqxclarity, don't toggle this.")
+@click.option('-d', '--update-dat', is_flag=True, help="Update the translated idx and dat file with the latest from Github. Requires the game to be closed.")
 
 
 def blast_off(
@@ -24,7 +23,6 @@ def blast_off(
     communication_window=False,
     player_names=False,
     npc_names=False,
-    disable_translations=False,
     community_logging=False,
     update_dat=False,
     debug=False,
@@ -37,32 +35,31 @@ def blast_off(
         download_custom_files()
 
     try:
-        if not disable_translations:
-            wait_for_dqx_to_launch()
+        wait_for_dqx_to_launch()
 
-            # Imports are done here as the program requires the game to be open otherwise.
-            # This allows us to test config and translate settings without launching everything.
-            from hooking.hook import activate_hooks
-            from clarity import loop_scan_for_walkthrough, run_scans
+        # Imports are done here as the program requires the game to be open otherwise.
+        # This allows us to test config and translate settings without launching everything.
+        from hooking.hook import activate_hooks
+        from clarity import loop_scan_for_walkthrough, run_scans
 
-            def start_process(name: str, target, args: tuple):
-                p = Process(name=name, target=target, args=args)
-                p.start()
-                time.sleep(.5)
-                while not p.is_alive():
-                    time.sleep(0.25)
+        def start_process(name: str, target, args: tuple):
+            p = Process(name=name, target=target, args=args)
+            p.start()
+            time.sleep(.5)
+            while not p.is_alive():
+                time.sleep(0.25)
 
-            if communication_window:
-                start_process(name="Hook loader", target=activate_hooks, args=(player_names,debug))
-                start_process(name="Walkthrough scanner", target=loop_scan_for_walkthrough, args=())
-            if community_logging:
-                logger.info("Thanks for enabling logging!")
-                threading.Thread(name="Community logging", target=start_logger, args=()).start()
+        if communication_window:
+            start_process(name="Hook loader", target=activate_hooks, args=(player_names,debug))
+            start_process(name="Walkthrough scanner", target=loop_scan_for_walkthrough, args=())
+        if community_logging:
+            logger.info("Thanks for enabling logging!")
+            threading.Thread(name="Community logging", target=start_logger, args=()).start()
 
-            start_process(name="Flavortown scanner", target=run_scans, args=(player_names, npc_names, debug))
-            # fmt: on
+        start_process(name="Flavortown scanner", target=run_scans, args=(player_names, npc_names, debug))
+        # fmt: on
 
-            logger.info("Done! Keep this window open (minimize it) and have fun on your adventure!")
+        logger.info("Done! Keep this window open (minimize it) and have fun on your adventure!")
     except Exception as e:
         logger.error(f"Can't find DQX process. Exiting. Error: {e}")
         sys.exit()
