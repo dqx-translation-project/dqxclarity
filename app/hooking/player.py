bc2fc72e012488bc2bea4c2306e940f24e75fa56
Serany <17505625+jmctune@users.noreply.github.com>
2023-10-02 19:39:41 -0500

    Load story_so_far entries from excel sheet into db.

--- app/hooking/player.py
+++ app/hooking/player.py
@@ -1,6 +1,8 @@
-from common.lib import encode_to_utf8, get_abs_path
+from common.lib import encode_to_utf8, get_abs_path, merge_jsons
 from common.memory import read_bytes, read_string, unpack_to_int
+from common.translate import convert_into_eng
 from json import dumps
+from openpyxl import load_workbook
 
 import os
 import sqlite3
@@ -15,15 +17,15 @@ class GetPlayer:
         else:
             self.address = unpack_to_int(address)
 
-        player = read_string(self.address + 24)
-        sibling = read_string(self.address + 96)
-        sibling_relationship = self.__determine_sibling_relationship()
+        self.ja_player_name = read_string(self.address + 24)
+        self.en_player_name = self.__get_en_player_name(player_name=self.ja_player_name)
+        self.ja_sibling_name = read_string(self.address + 96)
+        self.en_sibling_name = self.__get_en_player_name(player_name=self.ja_sibling_name)
+        self.sibling_relationship = self.__determine_sibling_relationship()
 
-        self.__write_db(
-            player_name=player,
-            sibling_name=sibling,
-            sibling_relationship=sibling_relationship
-        )
+        self.__write_player()
+        self.__load_dialog_into_db()
+
 
     def __determine_sibling_relationship(self):
         check_byte = read_bytes(self.address + 96 + 19, size=1)
@@ -37,16 +39,26 @@ class GetPlayer:
             return "younger_sister"
 
 
-    def __write_db(self, player_name: str, sibling_name: str, sibling_relationship: str):
+    def __get_en_player_name(self, player_name: str):
+        player_file = "/".join([get_abs_path(__file__), "../misc_files/custom_player_names.json"])
+        players = merge_jsons([player_file])
+
+        if player_name in players:
+            return players[player_name]
+
+        return convert_into_eng(word=player_name)
+
+
+    def __write_player(self):
         db_file = "/".join([get_abs_path(__file__), "../misc_files/clarity_dialog.db"])
 
         query = f"""
         BEGIN TRANSACTION;
         DELETE FROM player;
         INSERT INTO player (type, name) VALUES
-            ('player', '{player_name}'),
-            ('sibling', '{sibling_name}'),
-            ('sibling_relationship', '{sibling_relationship}');
+            ('player', '{self.ja_player_name}'),
+            ('sibling', '{self.ja_sibling_name}'),
+            ('sibling_relationship', '{self.sibling_relationship}');
         END TRANSACTION;
         """
 
@@ -55,6 +67,79 @@ class GetPlayer:
         cursor.executescript(query)
         conn.commit()
         cursor.close()
+
+
+    def __replace_with_en_names(self, string: str):
+        new_string = string.replace("<pnplacehold>", self.en_player_name)
+        new_string = new_string.replace("<snplacehold>", self.en_sibling_name)
+
+        if self.sibling_relationship in ["older_brother", "younger_brother"]:
+            new_string = new_string.replace("<kyodai_rel1>", "brother")
+            new_string = new_string.replace("<kyodai_rel2>", "brother")
+            new_string = new_string.replace("<kyodai_rel3>", "brother")
+        elif self.sibling_relationship in ["older_sister", "younger_sister"]:
+            new_string = new_string.replace("<kyodai_rel1>", "sister")
+            new_string = new_string.replace("<kyodai_rel2>", "sister")
+            new_string = new_string.replace("<kyodai_rel3>", "sister")
+
+        return new_string
+
+
+    def __replace_with_ja_names(self, string: str):
+        new_string = string.replace("<pnplacehold>", self.ja_player_name)
+        new_string = new_string.replace("<snplacehold>", self.ja_sibling_name)
+
+        if self.sibling_relationship == "older_brother":
+            new_string = new_string.replace("<kyodai_rel1>", "兄ちゃん")
+            new_string = new_string.replace("<kyodai_rel2>", "お兄ちゃん")
+            new_string = new_string.replace("<kyodai_rel3>", "兄")
+        elif self.sibling_relationship == "younger_brother":
+            new_string = new_string.replace("<kyodai_rel1>", "弟")
+            new_string = new_string.replace("<kyodai_rel2>", "弟")
+            new_string = new_string.replace("<kyodai_rel3>", "弟")
+        elif self.sibling_relationship == "older_sister":
+            new_string = new_string.replace("<kyodai_rel1>", "姉ちゃん")
+            new_string = new_string.replace("<kyodai_rel2>", "お姉ちゃん")
+            new_string = new_string.replace("<kyodai_rel3>", "姉")
+        elif self.sibling_relationship == "younger_sister":
+            new_string = new_string.replace("<kyodai_rel1>", "妹")
+            new_string = new_string.replace("<kyodai_rel2>", "妹")
+            new_string = new_string.replace("<kyodai_rel3>", "妹")
+
+        return new_string
+
+
+    def __load_dialog_into_db(self):
+        merge_file = "/".join([get_abs_path(__file__), "../misc_files/merge.xlsx"])
+        db_file = "/".join([get_abs_path(__file__), "../misc_files/clarity_dialog.db"])
+
+        #if os.path.exists(merge_file) and os.path.exists(db_file):
+        workbook = load_workbook(merge_file)
+        worksheet = workbook["Story So Far"]
+
+        conn = sqlite3.connect(db_file)
+        cursor = conn.cursor()
+
+        query = "DELETE FROM story_so_far"
+        cursor.execute(query)
+
+        for row_num in range(2, worksheet.max_row + 1):
+            ja_text = self.__replace_with_ja_names(
+                worksheet.cell(row=row_num, column=1).value.replace("'", "''"))
+
+            # if data in fixed english translation column, use it
+            if worksheet.cell(row=row_num, column=3).value:
+                en_text = self.__replace_with_en_names(
+                    worksheet.cell(row=row_num, column=3).value.replace("'", "''"))
+            else:
+                en_text = self.__replace_with_en_names(
+                    worksheet.cell(row=row_num, column=2).value.replace("'", "''"))
+
+            query = f"INSERT INTO story_so_far (ja, en) VALUES ('{ja_text}', '{en_text}')"
+            cursor.execute(query)
+
+        conn.commit()
+        conn.close()
 
 
 def player_name_shellcode(eax_address: int) -> str:
@@ -75,21 +160,3 @@ except Exception as e:
     """
 
     return encode_to_utf8(shellcode).decode()
-
-# 115 bytes - <kyodai_rel*> byte
-# 01 - older brother
-#     <kyodai_rel1>: 兄ちゃん
-#     <kyodai_rel2>: お兄ちゃん
-#     <kyodai_rel3>: 兄
-# 02 - younger brother
-#     <kyodai_rel1>: 弟
-#     <kyodai_rel2>: 弟
-#     <kyodai_rel3>: 弟
-# 03 - older sister
-#     <kyodai_rel1>: 姉ちゃん
-#     <kyodai_rel2>: お姉ちゃん
-#     <kyodai_rel3>: 姉
-# 04 - younger sister
-#     <kyodai_rel1>: 妹
-#     <kyodai_rel2>: 妹
-#     <kyodai_rel3>: 妹
