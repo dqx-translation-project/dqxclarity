3b75f9e516095556b5190963bbb6fc08e8cdf949
jmctune <17505625+jmctune@users.noreply.github.com>
2025-05-18 19:27:58 -0500

    add fixed signatures for 7.4

--- app/common/signatures.py
+++ app/common/signatures.py
@@ -5,20 +5,20 @@
 
 # takes you to the section of the function where you can read where dialog is stored before
 # it's rendered to screen. captures npc text.
-# FF 77 08 C7 45
-#    DQXGame.exe.text+432093 - 51                    - push ecx
-#    DQXGame.exe.text+432094 - 89 01                 - mov [ecx],eax
-#    DQXGame.exe.text+432096 - 8B C4                 - mov eax,esp
-#    DQXGame.exe.text+432098 - 89 30                 - mov [eax],esi
-#    DQXGame.exe.text+43209A - E8 41B8FFFF           - call DQXGame.exe.text+42D8E0
-# >> DQXGame.exe.text+43209F - FF 77 08              - push [edi+08]
-#    DQXGame.exe.text+4320A2 - C7 45 F0 00000000     - mov [ebp-10],00000000
-#    DQXGame.exe.text+4320A9 - C7 45 F4 FDFFFFFF     - mov [ebp-0C],FFFFFFFD
-#    DQXGame.exe.text+4320B0 - E8 CB0BC6FF           - call DQXGame.exe.text+92C80
-#    DQXGame.exe.text+4320B5 - 8B 15 18160D02        - mov edx,[DQXGame.exe+2011618]
-#    DQXGame.exe.text+4320BB - 83 C4 14              - add esp,14
-#    DQXGame.exe.text+4320BE - 89 45 F8              - mov [ebp-08],eax
-dialog_trigger = rb"\xFF\x77\x08\xC7\x45"
+# FF ?? ?? C7 45 ?? 00 00 00 00 C7 45 ?? FD FF FF FF E8
+#    DQXGame.exe+4F7F6F - 8D 4D C8              - lea ecx,[ebp-38]
+#    DQXGame.exe+4F7F72 - 56                    - push esi
+#    DQXGame.exe+4F7F73 - E8 186CCAFF           - call DQXGame.exe+19EB90
+#    DQXGame.exe+4F7F78 - 8D 4D C8              - lea ecx,[ebp-38]
+#    DQXGame.exe+4F7F7B - E8 107ACAFF           - call DQXGame.exe+19F990
+# >> DQXGame.exe+4F7F80 - FF 73 08              - push [ebx+08]
+# >> DQXGame.exe+4F7F83 - C7 45 F4 00000000     - mov [ebp-0C],00000000
+# >> DQXGame.exe+4F7F8A - C7 45 FC FDFFFFFF     - mov [ebp-04],FFFFFFFD
+# >> DQXGame.exe+4F7F91 - E8 8A22C4FF           - call DQXGame.exe+13A220
+#    DQXGame.exe+4F7F96 - 8B 15 C8C89302        - mov edx,[DQXGame.exe+1C2C8C8]
+#    DQXGame.exe+4F7F9C - 83 C4 04              - add esp,04
+#    DQXGame.exe+4F7F9F - 8B F8                 - mov edi,eax
+dialog_trigger = rb"\xFF..\xC7\x45.\x00\x00\x00\x00\xC7\x45.\xFD\xFF\xFF\xFF\xE8"
 
 # function that is triggered when a quest window opens. used for translating quest text
 # 8D 8E 78 04 00 00 E8 ?? ?? ?? ?? 5F
@@ -40,64 +40,49 @@ dialog_trigger = rb"\xFF\x77\x08\xC7\x45"
 quest_text_trigger = rb"\x8D\x8E\x78\x04\x00\x00\xE8....\x5F"
 
 # Integrity check + hooking addresses
-# 52 57 51 50 53 56
-# >> DQXGame.exe.text+DF049B - 52                    - push edx
-#    DQXGame.exe.text+DF049C - 57                    - push edi
-#    DQXGame.exe.text+DF049D - 51                    - push ecx
-#    DQXGame.exe.text+DF049E - 50                    - push eax
-#    DQXGame.exe.text+DF049F - 53                    - push ebx
-#    DQXGame.exe.text+DF04A0 - 56                    - push esi
-#    DQXGame.exe.text+DF04A1 - 8D 64 24 80           - lea esp,[esp--80]
-#    DQXGame.exe.text+DF04A5 - 66 0F11 3C 24         - movupd [esp],xmm7
-#    DQXGame.exe.text+DF04AA - 66 0F11 64 24 10      - movupd [esp+10],xmm4
-#    DQXGame.exe.text+DF04B0 - 66 0F11 44 24 20      - movupd [esp+20],xmm0
-#    DQXGame.exe.text+DF04B6 - 66 0F11 4C 24 30      - movupd [esp+30],xmm1
-#    DQXGame.exe.text+DF04BC - 66 0F11 54 24 40      - movupd [esp+40],xmm2
-#    DQXGame.exe.text+DF04C2 - 66 0F11 6C 24 50      - movupd [esp+50],xmm5
-#    DQXGame.exe.text+DF04C8 - 66 0F11 74 24 60      - movupd [esp+60],xmm6
-#    DQXGame.exe.text+DF04CE - 66 0F11 5C 24 70      - movupd [esp+70],xmm3
-#    DQXGame.exe.text+DF04D4 - E9 BF2754FD           - jmp DQXGame.exe.text+4C5C98
-integrity_check = rb"\x52\x57\x51\x50\x53\x56"
-
-# If we ever figure out how to go undetected with scans, this will translate
-# party names for us without scanning.
-# 8B CF FF 75 0C 53 50
-#    DQXGame.exe.text+E6B98 - 8B E5                 - mov esp,ebp
-#    DQXGame.exe.text+E6B9A - 5D                    - pop ebp
-#    DQXGame.exe.text+E6B9B - C2 1000               - ret 0010
-#    DQXGame.exe.text+E6B9E - FF 75 10              - push [ebp+10]
-# >> DQXGame.exe.text+E6BA1 - 8B CF                 - mov ecx,edi
-#    DQXGame.exe.text+E6BA3 - FF 75 0C              - push [ebp+0C]
-#    DQXGame.exe.text+E6BA6 - 53                    - push ebx
-#    DQXGame.exe.text+E6BA7 - 50                    - push eax
-#    DQXGame.exe.text+E6BA8 - E8 43D3FFFF           - call DQXGame.exe.text+E3EF0
-#    DQXGame.exe.text+E6BAD - 8B 4D FC              - mov ecx,[ebp-04]
-#    DQXGame.exe.text+E6BB0 - 5F                    - pop edi
-#    DQXGame.exe.text+E6BB1 - 5E                    - pop esi
-#    DQXGame.exe.text+E6BB2 - 33 CD                 - xor ecx,ebp
-menu_party_name_trigger = rb"\x8B\xCF\xFF\x75\x0C\x53\x50"
+# 8D 64 24 FC 8D 64 24 FC 89 0C 24 8D 64 24 FC 89 04 24 E9 ?? ?? ?? ?? 8D
+#    DQXGame.exe+727F6BF - 8D 64 24 04           - lea esp,[esp+04]
+# >> DQXGame.exe+727F6C3 - 8B 6C 24 FC           - mov ebp,[esp-04]
+# >> DQXGame.exe+727F6C7 - 8D 64 24 0C           - lea esp,[esp+0C]
+# >> DQXGame.exe+727F6CB - FF 64 24 F4           - jmp dword ptr [esp-0C]
+# >> DQXGame.exe+727F6CF - 89 54 24 FC           - mov [esp-04],edx
+# >> DQXGame.exe+727F6D3 - 8D 64 24 FC           - lea esp,[esp-04]
+# >> DQXGame.exe+727F6D7 - 8D 64 24 FC           - lea esp,[esp-04]
+# >> DQXGame.exe+727F6DB - 89 0C 24              - mov [esp],ecx
+# >> DQXGame.exe+727F6DE - 8D 64 24 FC           - lea esp,[esp-04]
+# >> DQXGame.exe+727F6E2 - 89 04 24              - mov [esp],eax
+# >> DQXGame.exe+727F6E5 - E9 9B6C1AF9           - jmp DQXGame.exe+426385
+# >> DQXGame.exe+727F6EA - 8D 45 F8              - lea eax,[ebp-08]
+#    DQXGame.exe+727F6ED - 89 44 24 FC           - mov [esp-04],eax
+#    DQXGame.exe+727F6F1 - 8D 64 24 FC           - lea esp,[esp-04]
+#    DQXGame.exe+727F6F5 - 8D 45 D4              - lea eax,[ebp-2C]
+#    DQXGame.exe+727F6F8 - 89 44 24 FC           - mov [esp-04],eax
+#    DQXGame.exe+727F6FC - 8D 64 24 FC           - lea esp,[esp-04]
+integrity_check = rb"\x8D\x64\x24\xFC\x8D\x64\x24\xFC\x89\x0C\x24\x8D\x64\x24\xFC\x89\x04\x24\xE9....\x8D"
 
 # a lot of network text that is drawn to the screen comes through this function
-# 8D 71 01 8B FF 8A 01 41 84 C0 75 F9 2B CE 51 51
-#    DQXGame.exe.text+42DF02 - 4E                    - dec esi
-#    DQXGame.exe.text+42DF03 - 75 EB                 - jne DQXGame.exe.text+42DEF0
-#    DQXGame.exe.text+42DF05 - 83 BD 70FDFFFF 10     - cmp dword ptr [ebp-00000290],10
-#    DQXGame.exe.text+42DF0C - 8D 95 5CFDFFFF        - lea edx,[ebp-000002A4]
-#    DQXGame.exe.text+42DF12 - 0F43 95 5CFDFFFF      - cmovae edx,[ebp-000002A4]
-#    DQXGame.exe.text+42DF19 - 8B CA                 - mov ecx,edx
-# >> DQXGame.exe.text+42DF1B - 8D 71 01              - lea esi,[ecx+01]
-#    DQXGame.exe.text+42DF1E - 8B FF                 - mov edi,edi
-#    DQXGame.exe.text+42DF20 - 8A 01                 - mov al,[ecx]
-#    DQXGame.exe.text+42DF22 - 41                    - inc ecx
-#    DQXGame.exe.text+42DF23 - 84 C0                 - test al,al
-#    DQXGame.exe.text+42DF25 - 75 F9                 - jne DQXGame.exe.text+42DF20
-#    DQXGame.exe.text+42DF27 - 2B CE                 - sub ecx,esi
-#    DQXGame.exe.text+42DF29 - 51                    - push ecx
-#    DQXGame.exe.text+42DF2A - 51                    - push ecx
-#    DQXGame.exe.text+42DF2B - 8B C4                 - mov eax,esp
-#    DQXGame.exe.text+42DF2D - 89 10                 - mov [eax],edx
-#    DQXGame.exe.text+42DF2F - EB 79                 - jmp DQXGame.exe.text+42DFAA
-network_text_trigger = rb"\x8D\x71\x01\x8B\xFF\x8A\x01\x41\x84\xC0\x75\xF9\x2B\xCE\x51\x51"
+# 8D 71 01 8A 01 41 84 C0 75 F9 2B CE 51 51
+#    DQXGame.exe+4F3E23 - 84 C0                 - test al,al
+#    DQXGame.exe+4F3E25 - 75 F9                 - jne DQXGame.exe+4F3E20
+#    DQXGame.exe+4F3E27 - EB 20                 - jmp DQXGame.exe+4F3E49
+#    DQXGame.exe+4F3E29 - 83 BD 78FCFFFF 0F     - cmp dword ptr [ebp-00000388],0F
+#    DQXGame.exe+4F3E30 - 8D 95 64FCFFFF        - lea edx,[ebp-0000039C]
+#    DQXGame.exe+4F3E36 - 0F47 95 64FCFFFF      - cmova edx,[ebp-0000039C]
+#    DQXGame.exe+4F3E3D - 8B CA                 - mov ecx,edx
+# >> DQXGame.exe+4F3E3F - 8D 71 01              - lea esi,[ecx+01]
+# >> DQXGame.exe+4F3E42 - 8A 01                 - mov al,[ecx]
+# >> DQXGame.exe+4F3E44 - 41                    - inc ecx
+# >> DQXGame.exe+4F3E45 - 84 C0                 - test al,al
+# >> DQXGame.exe+4F3E47 - 75 F9                 - jne DQXGame.exe+4F3E42
+# >> DQXGame.exe+4F3E49 - 2B CE                 - sub ecx,esi
+# >> DQXGame.exe+4F3E4B - 51                    - push ecx
+# >> DQXGame.exe+4F3E4C - 51                    - push ecx
+#    DQXGame.exe+4F3E4D - 8B C4                 - mov eax,esp
+#    DQXGame.exe+4F3E4F - 89 10                 - mov [eax],edx
+#    DQXGame.exe+4F3E51 - 8B CF                 - mov ecx,edi
+#    DQXGame.exe+4F3E53 - E8 B8D0CAFF           - call DQXGame.exe+1A0F10
+#    DQXGame.exe+4F3E58 - 8A D8                 - mov bl,al
+network_text_trigger = rb"\x8D\x71\x01\x8A\x01\x41\x84\xC0\x75\xF9\x2B\xCE\x51\x51"
 
 # player and sibling names on login. use this to figure out what the player is logged in as
 # 55 8B EC 56 8B F1 57 8B 46 58 85 C0
@@ -118,25 +103,6 @@ network_text_trigger = rb"\x8D\x71\x01\x8B\xFF\x8A\x01\x41\x84\xC0\x75\xF9\x2B\x
 # DQXGame.exe+422C95 - E8 F6ADC1FF           - call DQXGame.exe+3DA90
 player_sibling_name_trigger = rb"\x55\x8B\xEC\x56\x8B\xF1\x57\x8B\x46\x58\x85\xC0"
 
-# party member data hits this code. used to detour and overwrite name.
-# how it was found:
-# - Search in CE for one of your party members
-#   - Must be actual player entry - not just name
-# - "Find what writes here" on the name
-# - Used instructions right below "repe movsd" instruction
-# DQXGame.exe+5B7133 - F3 0F10 05 94F61601   - movss xmm0,[DQXGame.exe+E4F694]
-# DQXGame.exe+5B713B - 0F2F C1               - comiss xmm0,xmm1
-# DQXGame.exe+5B713E - 72 0C                 - jb DQXGame.exe+5B714C
-# DQXGame.exe+5B7140 - BE 01000000           - mov esi,00000001
-# DQXGame.exe+5B7145 - EB 05                 - jmp DQXGame.exe+5B714C
-# DQXGame.exe+5B7147 - BE 03000000           - mov esi,00000003
-# DQXGame.exe+5B714C - 3B FE                 - cmp edi,esi
-# DQXGame.exe+5B714E - B9 BE000000           - mov ecx,000000BE
-# DQXGame.exe+5B7153 - 8B 75 08              - mov esi,[ebp+08]
-# DQXGame.exe+5B7156 - 8D 7B 28              - lea edi,[ebx+28]
-# DQXGame.exe+5B7159 - F3 A5                 - repe movsd
-party_ai_trigger = rb"\x8B\x8B\xC8\x05\x00\x00"
-
 # top-right corner text from NPCs. seen primarily in v5/v6.
 # how it was found:
 # - Quest 764 has a way to test reproducing text in the top right
@@ -147,20 +113,35 @@ party_ai_trigger = rb"\x8B\x8B\xC8\x05\x00\x00"
 #   step and get her to say something new. Hope you picked an address
 #   that has a temporary string in it and was overwritten with the new
 #   string.
-#    DQXGame.exe.text+601020 - 55                    - push ebp
-#    DQXGame.exe.text+601021 - 8B EC                 - mov ebp,esp
-#    DQXGame.exe.text+601023 - 8B 45 08              - mov eax,[ebp+08]
-#    DQXGame.exe.text+601026 - 83 EC 10              - sub esp,10
-#    DQXGame.exe.text+601029 - 53                    - push ebx
-#    DQXGame.exe.text+60102A - 56                    - push esi
-#    DQXGame.exe.text+60102B - 8B F1                 - mov esi,ecx
-#    DQXGame.exe.text+60102D - 57                    - push edi
-#    DQXGame.exe.text+60102E - 85 C0                 - test eax,eax
-#    DQXGame.exe.text+601030 - 0F84 A1010000         - je DQXGame.exe.text+6011D7
-# >> DQXGame.exe.text+601036 - 8B D0                 - mov edx,eax
-#    DQXGame.exe.text+601038 - 8D 7A 01              - lea edi,[edx+01]
-#  8B D0 8D 7A 01 EB 03 8D 49 00 8A 0A 42 84 C9 75 F9 2B D7 0F 84 ?? ?? ?? ?? 51
-corner_text_trigger = rb"\x8B\xD0\x8D\x7A\x01\xEB\x03\x8D\x49\x00\x8A\x0A\x42\x84\xC9\x75\xF9\x2B\xD7\x0F\x84....\x51"
+#
+#    DQXGame.exe+6B2130 - 55                    - push ebp
+#    DQXGame.exe+6B2131 - 8B EC                 - mov ebp,esp
+#    DQXGame.exe+6B2133 - 8B 45 08              - mov eax,[ebp+08]
+#    DQXGame.exe+6B2136 - 83 EC 14              - sub esp,14
+#    DQXGame.exe+6B2139 - 53                    - push ebx
+#    DQXGame.exe+6B213A - 56                    - push esi
+#    DQXGame.exe+6B213B - 8B F1                 - mov esi,ecx
+#    DQXGame.exe+6B213D - 57                    - push edi
+#    DQXGame.exe+6B213E - 8B 7D 0C              - mov edi,[ebp+0C]
+#    DQXGame.exe+6B2141 - 85 C0                 - test eax,eax
+#    DQXGame.exe+6B2143 - 0F84 96010000         - je DQXGame.exe+6B22DF
+# >> DQXGame.exe+6B2149 - 8B D0                 - mov edx,eax
+# >> DQXGame.exe+6B214B - 8D 5A 01              - lea ebx,[edx+01]
+# >> DQXGame.exe+6B214E - 66 90                 - nop 2
+# >> DQXGame.exe+6B2150 - 8A 0A                 - mov cl,[edx]
+# >> DQXGame.exe+6B2152 - 42                    - inc edx
+# >> DQXGame.exe+6B2153 - 84 C9                 - test cl,cl
+# >> DQXGame.exe+6B2155 - 75 F9                 - jne DQXGame.exe+6B2150
+# >> DQXGame.exe+6B2157 - 2B D3                 - sub edx,ebx
+# >> DQXGame.exe+6B2159 - 0F84 80010000         - je DQXGame.exe+6B22DF
+#    DQXGame.exe+6B215F - 51                    - push ecx
+#    DQXGame.exe+6B2160 - 8B CC                 - mov ecx,esp
+#    DQXGame.exe+6B2162 - 89 01                 - mov [ecx],eax
+#    DQXGame.exe+6B2164 - 8D 45 F0              - lea eax,[ebp-10]
+#    DQXGame.exe+6B2167 - 50                    - push eax
+#    DQXGame.exe+6B2168 - 8B CE                 - mov ecx,esi
+# 8B D0 8D 5A 01 66 90 8A 0A 42 84 C9 75 F9 2B D3 0F
+corner_text_trigger = rb"\x8B\xD0\x8D\x5A\x01\x66\x90\x8A\x0A\x42\x84\xC9\x75\xF9\x2B\xD3\x0F"
 
 #############################################
 # "Patterns" seen to find various text.
