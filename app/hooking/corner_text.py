76f9fd6c26367e578d71358f6c2e9223f79abcbe
jmctune <17505625+jmctune@users.noreply.github.com>
2025-12-06 20:41:34 -0600

    read memory locally when running within the process.

--- app/hooking/corner_text.py
+++ app/hooking/corner_text.py
@@ -1,6 +1,6 @@
 from common.db_ops import generate_m00_dict
 from common.lib import get_project_root, setup_logger
-from common.memory import MemWriter
+from common.memory_local import MemWriterLocal
 from common.translate import detect_lang
 from json import dumps
 
@@ -9,30 +9,25 @@ import sys
 
 
 class CornerText:
-
-    misc_files = get_project_root("misc_files")
-    custom_text_logger = setup_logger("text_logger", get_project_root("logs/corner_text.log"))
-    writer = None
+    custom_text_logger = setup_logger(
+        "text_logger", get_project_root("logs/corner_text.log")
+    )
     data = None
 
-    def __init__(self, text_address: int, debug=False):
-        if not CornerText.writer:
-            CornerText.writer = MemWriter()
-        if debug:
-            self.text_address = text_address
-        else:
-            self.text_address = CornerText.writer.unpack_to_int(text_address)
-
+    def __init__(self, text_address: int):
         if CornerText.data is None:
             CornerText.data = generate_m00_dict("'custom_corner_text'")
 
-        text = CornerText.writer.read_string(self.text_address)
+        writer = MemWriterLocal()
+        text_address = writer.read_uint32(address=text_address, value=True)
+
+        text = writer.read_string(address=text_address)
 
         if detect_lang(text):
             if text in CornerText.data:
                 to_write = CornerText.data[text]
                 if to_write != "":
-                    CornerText.writer.write_string(self.text_address, to_write)
+                    writer.write_string(address=text_address, text=text)
             else:
                 CornerText.custom_text_logger.info(f"--\n>>corner_text ::\n{text}")
 
--- app/hooking/memchr.py
+++ app/hooking/memchr.py
@@ -1,6 +1,6 @@
 from common.db_ops import generate_m00_dict
 from common.lib import get_project_root, setup_logger
-from common.memory import MemWriter
+from common.memory_local import MemWriterLocal
 from json import dumps
 
 import os
@@ -9,36 +9,32 @@ import sys
 
 
 class MemChr:
-
-    writer = None
     jp_regex = regex.compile(r"\p{Script=Hiragana}|\p{Script=Katakana}|\p{Script=Han}")
-    custom_text_logger = setup_logger("memchr_logger", get_project_root("logs/memchr.log"))
+    custom_text_logger = setup_logger(
+        "memchr_logger", get_project_root("logs/memchr.log")
+    )
     m00_text = None
 
-    def __init__(self, esp_address: int, debug=False):
-        if not MemChr.writer:
-            MemChr.writer = MemWriter()
+    def __init__(self, esp_address: int):
         if not MemChr.m00_text:
             MemChr.m00_text = generate_m00_dict()
 
-        if debug:
-            self.esp_address = esp_address
-        else:
-            self.esp_address = MemChr.writer.unpack_to_int(esp_address)
+        writer = MemWriterLocal()
 
         # need to read the first argument on the stack (esp+4) to get the string to read.
-        self.text_address = MemChr.writer.proc.read_long(self.esp_address + 0x4)
-        self.text = MemChr.writer.read_string(self.text_address)
+        esp = writer.read_uint32(address=esp_address, value=True) + 0x4
+        text_addr = writer.read_ulong32(address=esp, value=True)
+        text = writer.read_string(address=text_addr)
 
-        if not self.__is_japanese(self.text):
+        if not self.__is_japanese(text):
             return
 
-        if self.text:
-            found = MemChr.m00_text.get(self.text)
+        if text:
+            found = MemChr.m00_text.get(text)
             if found:
                 # the original strength length (in bytes) must be the exact same size as what we write,
                 # or the game will throw an error code.
-                orig_length = len(self.text.encode("utf-8"))
+                orig_length = len(text.encode("utf-8"))
                 found_length = len(found.encode("utf-8"))
 
                 if found_length > orig_length:
@@ -48,9 +44,9 @@ class MemChr:
                     while len(found.encode("utf-8")) <= orig_length - 1:
                         found += " "
 
-                MemChr.writer.write_string(address=self.text_address, text=found)
+                writer.write_string(address=text_addr, text=found)
             else:
-                MemChr.custom_text_logger.info(f"---\nUncaptured:\n{self.text}")
+                MemChr.custom_text_logger.info(f"---\nUncaptured:\n{text}")
 
     def __is_japanese(cls, text: str):
         return bool(cls.jp_regex.search(text))
