db670c2926ab928a086835793301dcde449ddf84
Joey <17505625+jmctune@users.noreply.github.com>
2026-04-18 13:25:30 -0500

    don't make users write json; just simple text entry (#459)

---
+++ launcher/ViewModels/NamePair.cs
@@ -0,0 +1,19 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace DqxClarity.Launcher.ViewModels;
+
+public partial class NamePair : ObservableObject
+{
+    [ObservableProperty] private string _japanese = "";
+    [ObservableProperty] private string _preferred = "";
+
+    public IRelayCommand RemoveCommand { get; }
+
+    public NamePair(string japanese, string preferred, Action<NamePair> remove)
+    {
+        _japanese  = japanese;
+        _preferred = preferred;
+        RemoveCommand = new RelayCommand(() => remove(this));
+    }
+}
